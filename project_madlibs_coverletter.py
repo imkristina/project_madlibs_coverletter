25a98a1a1b1c0327044051cfa1dba968a513db0a
Kristina Vannest <33994673+imkristina@users.noreply.github.com>
2022-07-20 12:44:58 -0700

    Add files via upload

---
+++ project_madlibs_coverletter.py
@@ -0,0 +1,26 @@
+
+#fun resume cover letter.
+
+ceo = input("Name a CEO: ")
+profession = input("Name a Profession:")
+company_name = input("Name a company: ")
+adjective1 = input("adjective: ")
+adjective2 = input("adjective: ")
+adjective3 = input("adjective: ")
+number = input("Pick a number... any number: ")
+celebrity = input("Name a celebrity: ")
+quote = input("write a famous quote: ")
+name = input("Your name: ")
+
+coverletter = f"Dear {ceo},\
+ \nIt is with great enthusiasm that i apply for the {profession} role with {company_name}.\
+ \nI am a(n) {adjective1} candidate for the job because I am {adjective2} and {adjective3}. \
+ \nI have {number} year(s) experience in {profession}ing, but like {celebrity} says, {quote}.\
+ \nThank you for your consideration,\
+\n{name}"
+
+
+print(coverletter)
+
+
+    
